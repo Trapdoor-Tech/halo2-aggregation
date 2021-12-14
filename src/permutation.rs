bf4a8ece910fc87eb0033c1fdf1ec1bd23110cd2
woodstock-xkx <xkx@trapdoortech.com>
2021-12-14 20:31:24 +0800

    add expressions func for permutation

--- src/permutation.rs
+++ src/permutation.rs
@@ -4,7 +4,10 @@ use halo2::plonk::{ConstraintSystem, Error, VerifyingKey};
 use halo2::transcript::{EncodedChallenge, Transcript, TranscriptRead};
 use halo2wrong::circuit::ecc::AssignedPoint;
 use halo2wrong::circuit::AssignedValue;
-use std::io;
+use std::{io, iter};
+use halo2::poly::Rotation;
+use crate::{ChallengeBeta, ChallengeGamma, ChallengeX};
+use crate::multiopen::VerifierQuery;
 
 pub struct CommittedVar<C: CurveAffine> {
     // commitment of grand product polynomials of permutation argument
@@ -67,5 +70,63 @@ impl<C: CurveAffine> VerifyingKeyVar<C> {
         offset: &mut usize,
     ) -> Result<CommonEvaluatedVar<C>, Error> {
         unimplemented!()
+    }
+}
+
+impl<C: CurveAffine> EvaluatedVar<C> {
+    pub fn expressions<'a>(
+        &'a self,
+        common: &'a CommonEvaluatedVar<C>,
+        advice_evals: &'a [AssignedValue<C>],
+        fixed_evals: &'a [AssignedValue<C>],
+        instance_evals: &'a [AssignedValue<C>],
+        l_0: AssignedValue<C>,
+        l_last: AssignedValue<C>,
+        l_blind: AssignedValue<C>,
+        beta: ChallengeBeta<C>,
+        gamma: ChallengeGamma<C>,
+        x: ChallengeX<C>,
+    ) -> Result<Vec<AssignedValue<C>>, Error> {
+        unimplemented!()
+
+        // 1. l_0(x) * (1 - ZP_0(x)) = 0
+
+        // 2. l_last(X) * (ZP_l(x)^2 - ZP_l(x)) = 0
+
+        // 3. l_0(X) * (ZP_i(x) - ZP_{i-1}(omega^(last)*x)) = 0
+
+        // 4. (1 - (l_last(x) + l_blind(x))) * (
+        //       ZP_i(omega*x) \prod_k (p(x) + beta*sigma_k(x) + gamma)
+        //     - ZP_i(x) \prod_k (p(x) + beta * delta^k * x + gamma)
+        // )
+    }
+
+    pub fn queries(
+        &self,
+        blinding_factors: usize,
+    ) -> impl Iterator<Item = VerifierQuery<C>> + Clone {
+        let last_rot = Rotation((-blinding_factors+1) as i32);
+
+        iter::empty()
+            .chain(self.sets.iter().flat_map(move |set| {
+                iter::empty()
+                    .chain(Some(VerifierQuery::new(
+                        set.permutation_product_commitment.clone(),
+                        Rotation::cur(),
+                        set.permutation_product_eval.clone(),
+                    )))
+                    .chain(Some(VerifierQuery::new(
+                        set.permutation_product_commitment.clone(),
+                        Rotation::next(),
+                        set.permutation_product_next_eval.clone(),
+                    )))
+            }))
+            .chain(self.sets.iter().rev().skip(1).flat_map(move |set| {
+                Some(VerifierQuery::new(
+                    set.permutation_product_commitment.clone(),
+                    last_rot,
+                    set.permutation_product_last_eval.clone().unwrap(),
+                ))
+            }))
     }
 }
